0bd506ad066cb60b95577afe857da83dd8ecb93f
Erin2021 <appleyummy78@gmail.com>
2024-09-19 16:00:05 +0900

    server action으로 login logout 구현

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "lh3.googleusercontent.com",//단점:직접적이라 누구나 들어와서 사용할 수 있음. 그래서 해결법으로 유저가 로그인시 개인 storage에 넣어서 이미지 받아옴.중간다리 거쳐서 가져온다는 뜻
+      },
+    ],
+  },
+};
 
 export default nextConfig;
