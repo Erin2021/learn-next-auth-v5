0e4cac33fb4c284472546c7935560637b0728a4f
Erin2021 <appleyummy78@gmail.com>
2024-09-19 15:00:56 +0900

    oAuth(google,github) 구현

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,15 @@
-import NextAuth from "next-auth"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import prisma from "./lib/prisma"
- 
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "./lib/prisma";
+import Google from "next-auth/providers/google";
+import GitHub from "next-auth/providers/github";
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: PrismaAdapter(prisma),//db가 연결되었다.
-  providers: [],//로그인 제3자 제공자
-})
+  theme: {
+    logo: "/logo.png",
+  },
+  adapter: PrismaAdapter(prisma), //db가 연결되었다.
+  providers: [Google, GitHub], //로그인 제3자 제공자
+});
+
+//http://localhost:3000/api/auth/signin 에서 예시로그인 보는것-네가 커스텀해서 따로 만들수있음 이건 기본제공임
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -24,4 +24,4 @@ if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
 //Prisma는 기본적으로 데이터베이스 연결을 유지하기 위해 **연결 풀링(Connection Pooling)**을 사용하지만, Edge 환경에서는 이 방식이 적합하지 않을 수 있습니다. Neon 같은 서버리스 데이터베이스와의 연결을 위해 Edge-compatible 코드를 작성해야 하며, 이를 위해 서버리스에 최적화된 연결 방식(예: Neon의 Pool)을 사용하고 있습니다.
 //이 코드에서는 Prisma와 Neon을 Edge 환경에서 사용할 수 있도록 설정해 서버리스 환경에서 데이터베이스 연결을 효율적으로 처리할 수 있게 만듭니다.
 
-//문제는 edge-compatitbel코드를 작성했더니, prisma모듈증에 그걸 싫어하는 모듈이 있어->무시되는 에러인데, 해결안하고 deploy하면 짱 느려짐->해결법:nextauth버전이 업데이트되길기다려라ㅋㅋㅠㅠ->middleware.ts 때문에(edge에서 도는 것이기 때문) 그러니 일단 그것의 이름을 변경해서 에러를 없앤다
+//문제는 edge-compatitbe코드를 작성했더니, prisma모듈증에 그걸 싫어하는 모듈이 있어->무시되는 에러인데, 해결안하고 deploy하면 짱 느려짐->해결법:nextauth버전이 업데이트되길기다려라ㅋㅋㅠㅠ->middleware.ts 때문에(edge에서 도는 것이기 때문) 그러니 일단 그것의 이름을 변경해서 에러를 없앤다
