af3577409664f207c4324a552db138bc12f52ea4
Erin2021 <appleyummy78@gmail.com>
2024-09-19 16:51:36 +0900

    protected page 생성

--- src/_middleware.ts
+++ src/_middleware.ts
@@ -1,1 +1,4 @@
 export { auth as middleware } from "@/auth"//session이 30일 이후에는 유지가 되지 않을 것->30일뒤에 자동 로그아웃
+
+//option2 : handle authentication-excute everytime we open the page/secure이 덜하긴 함-그리고 middleware유저정보를 다른페이지에 전달할 수 없어서 귀찮음
+
--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,12 +1,19 @@
 import { Metadata } from "next";
 import SettingsPage from "./SettingsPage";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Settings",
 };
 
 export default async function Page() {
-  // TODO: Protect this page via authentication
+  // TODO: Protect this page via authentication-option1
+  const session = await auth();
+  const user = session?.user;
 
-  return <SettingsPage />;
+  if (!user) {
+    redirect("/api/auth/signin?callbackUrl=/settings");//signin하면 바로 settings화면으로 올수있게 하는 라우트(callbackUrl)
+  }
+  return <SettingsPage user={user}/>;
 }
